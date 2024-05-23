3c55a4ecab6277152d303184d883d969c5d38188
Klaudia <klaudia02460@gmail.com>
2024-05-23 17:52:31 +0200

    Add flights kafka producer

--- build.sbt
+++ build.sbt
@@ -1,9 +1,17 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "3.3.3"
+ThisBuild / scalaVersion := "2.12.18"
 
 lazy val root = (project in file("."))
   .settings(
     name := "FlightDelays",
     idePackagePrefix := Some("put.poznan.pl.klaudiak")
   )
+
+val sparkVersion = "3.5.0"
+libraryDependencies ++=Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion,
+  "org.apache.kafka" % "kafka-clients" % "3.7.0",
+)
---
+++ src/main/scala/producers/FlightsProducer.scala
@@ -0,0 +1,53 @@
+package put.poznan.pl.klaudiak
+package producers
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+import java.io.File
+import java.util.Properties
+import java.util.concurrent.TimeUnit
+import scala.io.Source
+
+object FlightsProducer extends App {
+
+  if (args.length != 4) {
+    println("Należy podać pięć parametrów: " +
+      "inputDir sleepTime topicName headerLength bootstrapServers")
+    System.exit(0)
+  }
+
+  val inputDir = args(0)
+  val sleepTime = args(1).toInt
+  val topicName = args(2)
+  val headerLength = args(3).toInt
+  val bootstrapServers = args(4)
+
+  val props = new Properties
+  props.put("bootstrap.servers", bootstrapServers)
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("acks", "all")
+  props.put("retries", 0)
+  props.put("batch.size", 16384)
+  props.put("linger.ms", 1)
+  props.put("buffer.memory", 33554432)
+
+  val producer = new KafkaProducer[String, String](props)
+
+  val folder = new File(inputDir)
+  val listOfFiles = folder.listFiles()
+  val listOfPaths = listOfFiles.map(_.getAbsolutePath).sorted
+
+  for (fileName <- listOfPaths) {
+    try {
+      val source = Source.fromFile(fileName)
+      val lines = source.getLines().drop(headerLength)
+      lines.foreach(line => producer.send(new ProducerRecord(topicName, line.hashCode.toString, line)))
+      TimeUnit.SECONDS.sleep(sleepTime)
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+  }
+
+  producer.close()
+}
